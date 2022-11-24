d92bd879778824a926fcf3008756757c38b69e8f
road2programmer <alberto.garrote@estudiantat.upc.edu>
2022-11-24 17:47:29 +0000

    test

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,6 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+module "s3_bucketd" {
+  source  = "app.terraform.io/agarrote-workshop-test/s3-bucket/aws"
+  version = "2.8.0"
   bucket_prefix = "agarrotemontes-viaplay"
   bucket = "my-s3-bucket"
   acl    = "private"
